02ce57ecb7dec91884fb9f52b5db281ba5d3251a
dchaffey <dchaffey@hortonworks.com>
2017-08-24 16:25:23 +0100

    Initial Package integrations with Travis, ReadTheDocs, PyUp, Github hooks

--- tests/test_nipyapi.py
+++ tests/test_nipyapi.py
@@ -23,4 +23,3 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
-    pass
