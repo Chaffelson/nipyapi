13545bcaa72560a2e629f3264746234392a03f59
dchaffey <dchaffey@hortonworks.com>
2017-08-24 17:53:22 +0100

    Added example swagger_client usage to main api file

--- nipyapi/nipyapi.py
+++ nipyapi/nipyapi.py
@@ -1,3 +1,9 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+# Example code:
+# import swagger_client
+# swagger_client.configuration.host = "http://localhost:8080/nifi-api"
+# con = swagger_client.SystemdiagnosticsApi()
+# con.get_system_diagnostics()
