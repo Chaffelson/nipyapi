c15adad6ee1861bd5afd7895167c49a9a0571449
dchaffey <dchaffey@hortonworks.com>
2017-09-21 11:20:02 +0100

    pathing bug in setup.py Added python 3.6 to tests

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open('docs/HISTORY.rst') as history_file:
+with open('docs/history.rst') as history_file:
     history = history_file.read()
 
 proj_version = '0.3.2'
