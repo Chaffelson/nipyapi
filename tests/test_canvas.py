06a3b0022661cd3598e45266eae1bd7623a5b94d
dchaffey <dchaffey@hortonworks.com>
2017-10-29 18:00:34 +0000

    Added test stubs for new Canvas functions Refactored requirements to not pin versions unnecessarily

--- tests/test_canvas.py
+++ tests/test_canvas.py
@@ -12,6 +12,10 @@ test_canvas = Canvas()
 
 
 class TestCanvas(object):
+    def test_get_root_pg_id(self):
+        r = test_canvas.get_root_pg_id()
+        assert isinstance(r, str)
+
     def test_process_group_status(self):
         r = test_canvas.process_group_status(pg_id='root', detail='names')
         assert isinstance(r, dict)
@@ -25,10 +29,12 @@ class TestCanvas(object):
         assert isinstance(r, dict)
         assert 'NiFi Flow' in r['name']
 
-    def test_get_root_pg_id(self):
-        r = test_canvas.get_root_pg_id()
-        assert isinstance(r, str)
-
     def test_list_all_process_groups(self):
         r = test_canvas.list_all_process_groups()
         assert isinstance(r, list)
+
+    def test_get_process_group_by_name(self):
+        pass
+
+    def test_delete_process_group(self):
+        pass
