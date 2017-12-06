cc6e48ae575a12f15cf1c4fd30fdbe3b2b796b89
dchaffey <dchaffey@hortonworks.com>
2017-12-06 18:18:36 +0000

    Bump version: 0.4.0 → 0.5.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('docs/history.rst') as history_file:
     history = history_file.read()
 
-proj_version = '0.4.0'
+proj_version = '0.5.0'
 
 requirements = [
     'certifi',
