eb7c123fbfcf3be8996b82b35fe40efc689c1dc4
dchaffey <dchaffey@hortonworks.com>
2017-12-07 21:50:48 +0000

    Bump version: 0.5.0 → 0.5.1

--- nipyapi/__init__.py
+++ nipyapi/__init__.py
@@ -6,5 +6,5 @@ NiPyAPI: A convenient Python wrapper for the Apache NiFi Rest API
 
 __author__ = """Daniel Chaffelson"""
 __email__ = 'chaffelson@gmail.com'
-__version__ = '0.5.0'
+__version__ = '0.5.1'
 __all__ = ['canvas', 'system', 'templates']
