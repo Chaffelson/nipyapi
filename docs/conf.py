9003dee5366c569fa90b6b31dc6f658063da7e88
Chaffelson <chaffelson@gmail.com>
2018-03-06 23:41:41 +0000

    removed m2r from sphinx requirements as well

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,6 @@
 
 import sys
 import os
-import m2r
 
 # If extensions (or modules to document with autodoc) are in another
 # directory, add these directories to sys.path here. If the directory is
