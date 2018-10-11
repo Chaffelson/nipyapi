8d9cd830e811485ea8044d94a79aa0c14a1a603b
Chaffelson <chaffelson@gmail.com>
2018-10-11 17:07:50 +0100

    Bump version: 0.10.3 → 0.11.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('docs/history.rst') as history_file:
     history = history_file.read()
 
-proj_version = '0.10.3'
+proj_version = '0.11.0'
 
 setup(
     name='nipyapi',
