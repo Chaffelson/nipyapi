828c6261d6efbd5bdd28f7517634226005f9a4c4
Justin Rittenhouse <jrittenh@nd.edu>
2019-01-18 16:48:23 -0500

    Add tests for create_service_user and create_service_user_group for nifi and registry

--- tests/test_security.py
+++ tests/test_security.py
@@ -6,7 +6,7 @@
 import pytest
 from tests import conftest
 import nipyapi
-
+from nipyapi import security
 
 def test_bootstrap_secured_nifi():
     pass
@@ -29,6 +29,66 @@ def test_get_access_policy_for_resource(regress_nifi):
     # Returns an error stating the NiFi isn't set up for this, rather than
     # the bad parameter error reported in issue #66
     with pytest.raises(ValueError, match='This NiFi is not configured'):
-        _ = nipyapi.security.get_access_policy_for_resource('flow', 'read')
+        _ = security.get_access_policy_for_resource('flow', 'read')
     # Note that on a secured NiFi with no valid policy you will get the error:
     # "No applicable policies could be found"
+
+
+def test_create_service_user_nifi():
+    nifi_user = security.create_service_user(
+        identity='testuser',
+        service='nifi'
+    )
+    assert isinstance(nifi_user, nipyapi.nifi.UserEntity)
+
+
+def test_create_service_user_registry():
+    registry_user = security.create_service_user(
+        identity='testuser',
+        service='registry'
+    )
+    assert isinstance(registry_user, nipyapi.registry.User)
+
+
+def test_create_service_user_group_nifi():
+    nifi_user_group = security.create_service_user_group(
+        identity='testusergroup',
+        service='nifi'
+    )
+    assert isinstance(nifi_user_group, nipyapi.nifi.UserGroupEntity)
+
+
+def test_create_service_user_group_registry():
+    registry_user_group = security.create_service_user_group(
+        identity='testusergroup',
+        service='registry'
+    )
+    assert isinstance(registry_user_group, nipyapi.registry.UserGroup)
+
+
+def test_create_service_user_group_with_users_nifi():
+    nifi_user = security.create_service_user_group(
+        identity='testuser',
+        service='nifi'
+    )
+    nifi_user_group = security.create_service_user_group(
+        identity='testusergroup',
+        service='nifi',
+        users=[nifi_user]
+    )
+    assert isinstance(nifi_user_group, nipyapi.nifi.UserGroupEntity)
+    assert nifi_user_group.users.len() == 1
+
+
+def test_create_service_user_group_with_users_registry():
+    registry_user = security.create_service_user_group(
+        identity='testuser',
+        service='registry'
+    )
+    registry_user_group = security.create_service_user_group(
+        identity='testusergroup',
+        service='registry',
+        users=[registry_user]
+    )
+    assert isinstance(registry_user_group, nipyapi.registry.UserGroup)
+    assert nifi_user_group.users.len() == 1
