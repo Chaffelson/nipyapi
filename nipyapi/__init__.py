c115237396a1dfa15cf1def783b321345a2fc633
Daniel Chaffelson <chaffelson@gmail.com>
2020-03-17 09:00:47 +0000

    Bump version: 0.14.0 → 0.14.1

--- nipyapi/__init__.py
+++ nipyapi/__init__.py
@@ -9,7 +9,7 @@ import importlib
 
 __author__ = """Daniel Chaffelson"""
 __email__ = 'chaffelson@gmail.com'
-__version__ = '0.14.0'
+__version__ = '0.14.1'
 __all__ = ['canvas', 'system', 'templates', 'config', 'nifi', 'registry',
            'versioning', 'demo', 'utils', 'security']
 
