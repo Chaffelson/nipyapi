14028a045628e4b646c62de9722c7bd913c895a6
Chris Sampson <12159006+ChrisSamo632@users.noreply.github.com>
2022-05-12 14:29:24 +0100

    Allow version validation to ignore NiFi or Registry when checking for parameter support (#304)
    * Allow version validation to ignore NiFi or Registry when checking for parameter support
    
    * Refactor if-else statement format

--- nipyapi/utils.py
+++ nipyapi/utils.py
@@ -530,11 +530,23 @@ def check_version(base, comparator=None, service='nifi'):
     return 0
 
 
-def validate_parameters_versioning_support():
-    """Convenience method to check if Parameters are supported"""
-    nifi_check = enforce_min_ver('1.10', bool_response=True)
-    registry_check = enforce_min_ver(
-        '0.6', service='registry', bool_response=True)
+def validate_parameters_versioning_support(verify_nifi=True, verify_registry=True):
+    """
+    Convenience method to check if Parameters are supported
+    Args:
+        verify_nifi (bool): If True, check NiFi meets the min version
+        verify_registry (bool): If True, check Registry meets the min version
+    """
+    if verify_nifi:
+        nifi_check = enforce_min_ver('1.10', bool_response=True)
+    else:
+        nifi_check = False
+
+    if verify_registry:
+        registry_check = enforce_min_ver('0.6', service='registry', bool_response=True)
+    else:
+        registry_check = False
+
     if nifi_check or registry_check:
         log.warning("Connected NiFi Registry may not support "
                     "Parameter Contexts and they may be lost in "
