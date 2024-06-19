8ec8fb06b9554e9c42d569a08b512d0781478cf9
Michaël Arnauts <michael.arnauts@dstny.com>
2024-06-19 13:26:09 +0200

    Fix parameter_sensitivities checking the values instead of the keys

--- nipyapi/nifi/models/parameter_group_configuration_entity.py
+++ nipyapi/nifi/models/parameter_group_configuration_entity.py
@@ -130,10 +130,10 @@ class ParameterGroupConfigurationEntity(object):
         :type: dict(str, str)
         """
         allowed_values = ["SENSITIVE", "NON_SENSITIVE"]
-        if not set(parameter_sensitivities.keys()).issubset(set(allowed_values)):
+        if not set(parameter_sensitivities.values()).issubset(set(allowed_values)):
             raise ValueError(
-                "Invalid keys in `parameter_sensitivities` [{0}], must be a subset of [{1}]"
-                .format(", ".join(map(str, set(parameter_sensitivities.keys())-set(allowed_values))),
+                "Invalid values in `parameter_sensitivities` [{0}], must be a subset of [{1}]"
+                .format(", ".join(map(str, set(parameter_sensitivities.values())-set(allowed_values))),
                         ", ".join(map(str, allowed_values)))
             )
 
