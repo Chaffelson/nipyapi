3f5e8fdba139a8b3a979002e72ed3d7f04fa8c1b
Michaël Arnauts <michael.arnauts@dstny.com>
2024-06-19 11:27:24 +0200

    Fix API model generator

--- resources/client_gen/generate_api_client.sh
+++ resources/client_gen/generate_api_client.sh
@@ -10,13 +10,13 @@
 echo Exporting Params
 export wv_client_name=${wv_client_name:-nifi}
 
-export wv_codegen_filename=${wv_codegen_filename:-swagger-codegen-cli-2.3.1.jar}
+export wv_codegen_filename=${wv_codegen_filename:-swagger-codegen-cli-2.4.41.jar}
 export wv_tmp_dir=${wv_tmp_dir:-${HOME}/Projects/tmp}
 export wv_client_dir=${wv_tmp_dir}/${wv_client_name}
 export wv_mustache_dir=./swagger_templates
 export wv_api_def_dir=./api_defs
 
-export wv_codegen_url=central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.3.1/${wv_codegen_filename}
+export wv_codegen_url=https://repo1.maven.org/maven2/io/swagger/swagger-codegen-cli/2.4.41/${wv_codegen_filename}
 export wv_swagger_def=$(ls ${wv_api_def_dir} | grep ${wv_client_name} | sort -V | tail -1)
 
 echo Prepping Workspace
