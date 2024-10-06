ac9898ef37adb2a5af67a3f59a9160e0111fa4d9
Dan Chaffelson <chaffelson@gmail.com>
2024-10-06 13:15:51 +0000

    correct version in nipyapi/__init__.py

--- nipyapi/__init__.py
+++ nipyapi/__init__.py
@@ -9,7 +9,7 @@ import importlib
 
 __author__ = """Daniel Chaffelson"""
 __email__ = 'chaffelson@gmail.com'
-__version__ = '0.19.1'
+__version__ = '0.20.0'
 __all__ = ['canvas', 'system', 'templates', 'config', 'nifi', 'registry',
            'versioning', 'demo', 'utils', 'security', 'parameters']
 
